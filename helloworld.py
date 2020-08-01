35eeedb779c2810852929b34d49dfe2fe2702bec
Yikuan Sun <yikuansun2015@gmail.com>
2020-08-01 10:58:18 -0400

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,10 +1,1 @@
-class HelloWorld:
-    def __init__(self):
-        self.message = "Hello world!"
-
-    def __str__(self):
-        return self.message
-
-if __name__ == "__main__":
-    myMessage = HelloWorld()
-    print(myMessage)
+print("hello worl'")
