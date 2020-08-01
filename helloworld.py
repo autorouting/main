5367a3a1a9aa1f3816591020915c323f181341ea
chunjinlu <chunjinlu@gmail.com>
2020-08-01 16:18:44 -0400

    Test change
    Test by Chunjin

--- helloworld.py
+++ helloworld.py
@@ -1,6 +1,6 @@
 class HelloWorld:
     def __init__(self):
-        self.message = "Hello world!"
+        self.message = "Hello world! We made some changes"
 
     def __str__(self):
         return self.message
