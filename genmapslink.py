506c9ec24354586d948da5df96abe7a000a16318
Yikuan Sun <yikuansun2015@gmail.com>
2020-08-05 13:54:16 -0400

    updated genmapslink
    made it run the other program automatically. epic!

--- genmapslink.py
+++ genmapslink.py
@@ -1,3 +1,4 @@
+exec(open("onevehicleroutegen.py").read())
 in_file = open("route.txt", "r")
 route = in_file.read().split(" -> ")
 # route is a text file in the same format as printed from the program
