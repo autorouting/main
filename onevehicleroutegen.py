26c09f5adb1ce9b763100ae22b53a3a61217f64c
NinjadenMu <62183972+Ninjaden1@users.noreply.github.com>
2020-08-06 15:46:09 -0400

    Update onevehicleroutegen.py

--- onevehicleroutegen.py
+++ onevehicleroutegen.py
@@ -38,42 +38,74 @@ def create_data_model():
     data['depot'] = 0
     return data
 
-def print_solution(manager, routing, solution):
-    print('Objective: {} miles'.format(solution.ObjectiveValue()))
-    index = routing.Start(0)
-    plan_output = 'Route for vehicle 0:\n'
-    route_distance = 0
-    textfileoutput = ""
-    while not routing.IsEnd(index):
-        plan_output += ' {} ->'.format(addresses[manager.IndexToNode(index)])
-        textfileoutput += ' {} ->'.format(addresses[manager.IndexToNode(index)])
-        previous_index = index
-        index = solution.Value(routing.NextVar(index))
-        route_distance += routing.GetArcCostForVehicle(previous_index, index, 0)
-    plan_output += ' {}\n'.format(addresses[manager.IndexToNode(index)])
-    textfileoutput += ' {}\n'.format(addresses[manager.IndexToNode(index)])
-    print(plan_output)
-    outputfile = open("route.txt", "w")
-    outputfile.write(textfileoutput)
-    outputfile.close()
-    plan_output += 'Route distance: {}miles\n'.format(route_distance)
+def print_solution(data, manager, routing, solution):
+    max_route_distance = 0
+    for vehicle_id in range(data['num_vehicles']):
+        index = routing.Start(vehicle_id + 1)
+        plan_output = 'Route for vehicle {}:\n'.format(vehicle_id)
+        route_distance = 0
+        while not routing.IsEnd(index):
+            plan_output += ' {} -> '.format(manager.IndexToNode(index))
+            previous_index = index
+            index = solution.Value(routing.NextVar(index))
+            route_distance += routing.GetArcCostForVehicle(
+                previous_index, index, vehicle_id)
+        plan_output += '{}\n'.format(manager.IndexToNode(index))
+        plan_output += 'Distance of the route: {}m\n'.format(route_distance)
+        print(plan_output)
+        max_route_distance = max(route_distance, max_route_distance)
+    print('Maximum of the route distances: {}m'.format(max_route_distance))
+
+
+
 
 def main():
+    # Instantiate the data problem.
     data = create_data_model()
+
+    # Create the routing index manager.
     manager = pywrapcp.RoutingIndexManager(len(data['distance_matrix']),
-                                              data['num_vehicles'], data['depot'])
+                                           data['num_vehicles'], data['depot'])
+
+    # Create Routing Model.
     routing = pywrapcp.RoutingModel(manager)
+
+
+    # Create and register a transit callback.
     def distance_callback(from_index, to_index):
+        """Returns the distance between the two nodes."""
+        # Convert from routing variable Index to distance matrix NodeIndex.
         from_node = manager.IndexToNode(from_index)
         to_node = manager.IndexToNode(to_index)
         return data['distance_matrix'][from_node][to_node]
+
     transit_callback_index = routing.RegisterTransitCallback(distance_callback)
+
+    # Define cost of each arc.
     routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
+
+    # Add Distance constraint.
+    dimension_name = 'Distance'
+    routing.AddDimension(
+        transit_callback_index,
+        0,  # no slack
+        10000,  # vehicle maximum travel distance
+        True,  # start cumul to zero
+        dimension_name)
+    distance_dimension = routing.GetDimensionOrDie(dimension_name)
+    distance_dimension.SetGlobalSpanCostCoefficient(100)
+
+    # Setting first solution heuristic.
     search_parameters = pywrapcp.DefaultRoutingSearchParameters()
     search_parameters.first_solution_strategy = (routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
+
+    # Solve the problem.
     solution = routing.SolveWithParameters(search_parameters)
+
+    # Print solution on console.
     if solution:
-        print_solution(manager, routing, solution)
+        print_solution(data, manager, routing, solution)
+
 
 if __name__ == '__main__':
     main()
