62821add842850fff98d45f2f934568448feb876
Yichen Sun <50383075+Tigersteve123@users.noreply.github.com>
2020-08-07 13:23:17 -0400

    Update README and InstallDependencies.sh

--- InstallDependencies.sh
+++ InstallDependencies.sh
@@ -5,5 +5,5 @@ conda config --prepend channels conda-forge
 conda create -n ox --strict-channel-priority osmnx
 conda activate ox
 
-# echo "Install ortools, geopy, networkx"
+echo "Install ortools, geopy, networkx"
 pip3 install ortools geopy networkx
