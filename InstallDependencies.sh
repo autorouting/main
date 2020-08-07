5a2e232d2ddbb10b1bb239f332ca5bd02b248235
Yichen Sun <50383075+Tigersteve123@users.noreply.github.com>
2020-08-07 13:19:58 -0400

    Update InstallDependencies.sh

--- InstallDependencies.sh
+++ InstallDependencies.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-echo "Install conda on Python3"
-python3 -m pip install conda
-
 echo "Setup virtual environment"
 conda config --prepend channels conda-forge
 conda create -n ox --strict-channel-priority osmnx
 conda activate ox
 
-# echo "Install ortools, geopy, networkx on Python3"
+# echo "Install ortools, geopy, networkx"
 pip3 install ortools geopy networkx
