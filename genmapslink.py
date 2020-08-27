21f0ac64eff125c0ce2c190d0624dca1af58c52c
NinjadenMu <62183972+Ninjaden1@users.noreply.github.com>
2020-08-26 21:18:22 -0400

    Update genmapslink.py

--- genmapslink.py
+++ genmapslink.py
@@ -1,16 +1,16 @@
-import webbrowser
-
 #exec(open("onevehicleroutegen.py").read())
-in_file = open("route.txt", "r")
-routes = in_file.read().split("\n")
-in_file.close()
-for i in range(len(routes)):
+def maps_link():
+    in_file = open("route.txt", "r")
+    routes = in_file.read().split("\n")
+    in_file.close()
+    
+    for i in range(len(routes)):
 
-    if routes[i] != "" and routes[i] != " ":
-        route = routes[i].split(" -> ")
-        outstring = "https://www.google.com/maps/dir/"
-        for j in range(len(route)):
-            outstring += route[j].replace(" ", "+") + "/"
-        print("\nGoogle Maps link for vehicle {}: {}\n".format(i, outstring))
-        
-        webbrowser.open(outstring, new=2)
+        if routes[i] != "" and routes[i] != " " and routes[i] != None:
+            route = routes[i].split(" -> ")
+            outstring = "https://www.google.com/maps/dir/"
+            for j in range(len(route)):
+                outstring += route[j].replace(" ", "+") + "/"
+    output = "Google Maps link for vehicle {}: {}\n".format(i, outstring)
+    print(output)
+    return output
