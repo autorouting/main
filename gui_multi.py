c172bfea196d41a1ef8e404341d5c753d7bfb3d5
DissmoBob <45155704+DissmoBob@users.noreply.github.com>
2020-08-28 22:22:06 -0400

    Update gui_multi.py
    Better functioning

--- gui_multi.py
+++ gui_multi.py
@@ -3,11 +3,12 @@ from tkinter import messagebox
 from tkinter.scrolledtext import ScrolledText
 import random
 import multi_vehicle_routegen as mvr
-
+import webbrowser
+import genmapslink
 root = Tk()
 root.title("Autorouting app")
 
-label1 = Label(root, text="City, County, or State (choose the smallest one that encompasses all locations):")
+label1 = Label(root, text="City, County, or State:")
 label1.pack()
 
 citybox = Entry(root, width=50)
@@ -37,21 +38,45 @@ def validate():
 
 def launch():
     if validate():
+        global activation
+        activation = [False]
+        driveroutput = driveraddressbox.get("1.0", END)
+        while driveroutput[-1] in ["\n","\t","r"]: driveroutput = driveroutput[:-1]
+        consumeroutput = consumeraddressbox.get("1.0", END)
+        while consumeroutput[-1] in ["\n","\t","r"]: consumeroutput = consumeroutput[:-1]
         #save driver
-        with open("driver_home_addresses.txt", "w") as drivertextfile: drivertextfile.write(driveraddressbox.get("1.0", END)[:-1])
+        with open("driver_home_addresses.txt", "w") as drivertextfile: drivertextfile.write(driveroutput)
         
         #save consummer
-        with open("locations.txt", "w") as locationstextfile: locationstextfile.write(restaurantaddressbox.get().replace("\n", "") + "\n" + consumeraddressbox.get('1.0', END)[:-1])
+        with open("locations.txt", "w") as locationstextfile: locationstextfile.write(restaurantaddressbox.get().replace("\n", "") + "\n" + consumeroutput)
         mvr.city_name = citybox.get()
+        for widget in root.winfo_children():
+            widget.destroy()
         loading = Label(root, text="Loading...")
         loading.pack()
         for widget in root.winfo_children():
             widget.destroy()
-        #code_to_exec = open("onevehicleroutegen.py").read().replace('input("city (ex.: Piedmont, California, USA):\\n ")', "'" + city + "'").replace('input("Your app name:\\n ")', "'" + str(random.randint(0, 999)) + str(random.randint(0, 999)) + "'") + "\n    exec(open('genmapslink.py').read())"
-        #exec(code_to_exec)
+        def callback(routelink):
+            print("hey")
+            global activation
+            if activation[0]: webbrowser.open(routelink, 0)
+        buttons = []
+        displayroutes = []
+        functions = [None]
         routes = mvr.main()
-        output = Label(root, text=routes)
-        output.pack()
+        for widget in root.winfo_children():
+            widget.destroy()
+        outputroutes = routes.split("\n")
+        for outputting in range(len(outputroutes)):
+            displayroutes.append(Label(root, text=(outputroutes[outputting].replace("\n", "\n\n")).replace(" -> ", " ->\n")))
+            displayroutes[-1].pack()
+            #def DIE(): callback(genmapslink.maps_link(outputting))
+            if outputting != 0:
+                buttons.append(Button(root, text="Open Google Maps link in browser", command=callback(genmapslink.maps_link(outputting))))
+                buttons[-1].pack()
+        activation[0] = True
+            
+        
     else: messagebox.showwarning(title="Warning", message="Please fill in every box.")
 
 myButton = Button(root, text="Launch program", command=launch)
