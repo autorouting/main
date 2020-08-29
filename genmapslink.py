26700b3e9a2d451e28303963c218c3c6d883bfba
DissmoBob <45155704+DissmoBob@users.noreply.github.com>
2020-08-28 22:24:28 -0400

    Update genmapslink.py
    Added multi vehicle functionality

--- genmapslink.py
+++ genmapslink.py
@@ -1,16 +1,24 @@
 #exec(open("onevehicleroutegen.py").read())
-def maps_link():
+def maps_link(x=-1):
     in_file = open("route.txt", "r")
     routes = in_file.read().split("\n")
     in_file.close()
-    
-    for i in range(len(routes)):
+    if x==-1:
+        for i in range(len(routes)):
 
-        if routes[i] != "" and routes[i] != " " and routes[i] != None:
-            route = routes[i].split(" -> ")
+            if routes[i] != "" and routes[i] != " " and routes[i] != None:
+                route = routes[i].split(" -> ")
+                outstring = "https://www.google.com/maps/dir/"
+                for j in range(len(route)):
+                    outstring += route[j].replace(" ", "+") + "/"
+                output = "Google Maps link for vehicle {}: {}\n".format(i, outstring)
+
+    else:
+        if routes[x] != "" and routes[x] != " " and routes[x] != None:
+            route = routes[x].split(" -> ")
             outstring = "https://www.google.com/maps/dir/"
             for j in range(len(route)):
                 outstring += route[j].replace(" ", "+") + "/"
-    output = "Google Maps link for vehicle {}: {}\n".format(i, outstring)
+            output = "Google Maps link for vehicle {}: {}\n".format(x, outstring)
     print(output)
     return outstring
