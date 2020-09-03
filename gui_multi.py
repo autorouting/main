382cdea43bd2224849a06be439271a4234e9c979
DissmoBob <45155704+DissmoBob@users.noreply.github.com>
2020-09-02 21:40:08 -0400

    Update gui_multi.py
    Added comments very cool :cool:

--- gui_multi.py
+++ gui_multi.py
@@ -1,3 +1,4 @@
+# [START import]
 from tkinter import *
 from tkinter import messagebox
 from tkinter.scrolledtext import ScrolledText
@@ -6,6 +7,10 @@ import multi_vehicle_routegen as mvr
 import webbrowser
 import genmapslink
 from functools import partial
+# [END import]
+
+# [START input form objects]
+# These are labels and textboxes on the GUI input form.
 root = Tk()
 root.title("Autorouting app")
 
@@ -32,15 +37,18 @@ label4.pack()
 
 consumeraddressbox = ScrolledText(root, width=50,height=12)
 consumeraddressbox.pack()
+# [END input form objects]
 
+#Verify if textboxes on the gui are empty or not
 def validate():
     if citybox.get() == "" or restaurantaddressbox.get()=="" or len(driveraddressbox.get("1.0", END)) == 0 or len(consumeraddressbox.get("1.0", END))== 0: return False
     else: return True
 
+#Launch routing
 def launch():
+    #if all information are provided, proceed with distances calculating
     if validate():
         global activation
-        #FIX activation stuff
         activation = [True]
         driveroutput = driveraddressbox.get("1.0", END)
         while driveroutput[-1] in ["\n","\t","r"]: driveroutput = driveroutput[:-1]
@@ -49,7 +57,7 @@ def launch():
         #save driver
         with open("driver_home_addresses.txt", "w") as drivertextfile: drivertextfile.write(driveroutput)
         
-        #save consummer
+        #save consumer
         with open("locations.txt", "w") as locationstextfile: locationstextfile.write(restaurantaddressbox.get().replace("\n", "") + "\n" + consumeroutput)
         mvr.city_name = citybox.get()
         for widget in root.winfo_children():
@@ -59,7 +67,6 @@ def launch():
         for widget in root.winfo_children():
             widget.destroy()
         def callback(routelink):
-            #print("hey")
             global activation
             if activation[0]: webbrowser.open(routelink, 0)
         buttons = []
@@ -72,20 +79,22 @@ def launch():
         for outputting in range(len(outputroutes)):
             displayroutes.append(Label(root, text=(outputroutes[outputting].replace("\n", "\n\n")).replace(" -> ", " ->\n")))
             displayroutes[-1].pack()
-            #def DIE(): callback(genmapslink.maps_link(outputting))
             if outputting != 0:
                 print(partial(genmapslink.maps_link, outputting)())
                 buttons.append(Button(root, text="Open Google Maps link in browser", command=partial(callback, partial(genmapslink.maps_link, outputting)())))
                 buttons[-1].pack()
         activation[0] = True
             
-        
+    #if any input box is empty, display a message box     
     else: messagebox.showwarning(title="Warning", message="Please fill in every box.")
 
+# [START buttons on the input form]
 myButton = Button(root, text="Launch program", command=launch)
 myButton.pack()
 
 bottomtext = Label(root, text="Find us on GitHub: https://github.com/autorouting/main")
 bottomtext.pack()
+# [END buttons on the input form]
 
+# call the main function
 root.mainloop()
