c064037afa635670a440ed8f3625d3a9d018ceae
Yikuan Sun <yikuansun2015@gmail.com>
2020-09-04 17:39:42 -0400

    added comments
    yes

--- gui.py
+++ gui.py
@@ -5,17 +5,11 @@ import onevehicleroutegen
 import genmapslink
 import webbrowser
 
+# Initiate Tkinter
 root = Tk()
 root.title("Autorouting app (one-vehicle)")
 
-"""
-label1 = Label(root, text="City, County, or State (choose the smallest one that encompasses all locations):")
-label1.pack()
-
-citybox = Entry(root, width=50)
-citybox.pack()
-"""
-
+# Build display
 label2 = Label(root, text="Driver address:")
 label2.pack()
 
@@ -34,36 +28,43 @@ label4.pack()
 consumeraddressbox = ScrolledText(root, width=50,height=30)
 consumeraddressbox.pack()
 
+# main
 def launch():
     global route_solution
+
+    # Write inputs to communication file
     locationstextfile = open("locations.txt", "w")
     locationstextfile.write(driveraddressbox.get().replace("\n", "") + "\n" + restrauntaddressbox.get().replace("\n", "") + "\n" + consumeraddressbox.get('1.0', END))
     locationstextfile.close()
-    #city = citybox.get()
+
+    # Destroy previous display
     for widget in root.winfo_children():
         widget.destroy()
     loading = Label(root, text="Loading...")
     loading.pack()
-    #code_to_exec = open("onevehicleroutegen.py").read().replace('input("city (ex.: Piedmont, California, USA):\\n ")', "'" + city + "'").replace('input("Your app name:\\n ")', "'" + str(random.randint(0, 999)) + str(random.randint(0, 999)) + "'") + "\n    exec(open('genmapslink.py').read())"
-    #exec(code_to_exec)
 
+    # Communicate with main program
     route_solution = onevehicleroutegen.main()
     for widget in root.winfo_children():
         widget.destroy()
 
+    # Generate output text
     display_route = Label(root, text=route_solution.replace(" -> ", " ->\n"))
     display_route.pack()
+
+    # Generate output link
     route_link = genmapslink.maps_link()
     def callback():
-        webbrowser.open(route_link, 0)
+        webbrowser.open(route_link, 0) # open link in browser if callback is called
 
     button_link = Button(root, text="Open Google Maps link in browser", command=callback)
     button_link.pack()
 
+# Finish main display
 myButton = Button(root, text="Launch program", command=launch)
 myButton.pack()
 
 bottomtext = Label(root, text="Find us on GitHub: https://github.com/autorouting/main")
 bottomtext.pack()
 
-root.mainloop()
+root.mainloop() # required for Tkinter
