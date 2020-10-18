57f22ed31c54c2b4e7928f99eade5a284219aa6b
Yikuan Sun <yikuansun2015@gmail.com>
2020-10-17 21:01:46 -0400

    moved the code
    bc why not?!?!?

--- webgui.py
+++ webgui.py
@@ -3,6 +3,18 @@ import cgi, cgitb
 import onevehicleroutegen_web
 import genmapslink_web
 
+# get the inputs
+form = cgi.FieldStorage()
+api_key = form.getvalue("api_key")
+driver_address = form.getvalue("driver")
+restaurant_address = form.getvalue("restaurant")
+consumer_addresses = form.getvalue("consumer")
+ 
+# Write inputs to communication file
+locationstextfile = open("locations.txt", "w")
+locationstextfile.write(driver_address + "\n" + restaurant_address + "\n" + consumer_addresses)
+locationstextfile.close()
+
 # change to HTML display
 print("Content-type:text/html\n")
 print()
@@ -16,18 +28,6 @@ body {
     font-family: Arial, Helvetica, sans-serif;
 }
 """)
-
-# get the inputs
-form = cgi.FieldStorage()
-api_key = form.getvalue("api_key")
-driver_address = form.getvalue("driver")
-restaurant_address = form.getvalue("restaurant")
-consumer_addresses = form.getvalue("consumer")
-
-# Write inputs to communication file
-locationstextfile = open("locations.txt", "w")
-locationstextfile.write(driver_address + "\n" + restaurant_address + "\n" + consumer_addresses)
-locationstextfile.close()
 
 route_solution = onevehicleroutegen_web.main(api_key)
 route_link = genmapslink_web.maps_link()
