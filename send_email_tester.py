1248bef990faed750dc86672bd2aaba339fc2bb9
NinjadenMu <jaden.mu@gmail.com>
2020-11-22 11:18:35 -0500

    Update send_email_tester.py

--- send_email_tester.py
+++ send_email_tester.py
@@ -2,5 +2,5 @@ import send_email
 
 credentials = str(open("email_config.txt", "r").read())
 credentials = credentials.split('\n')
-send_email.send_email(credentials[0], credentials[1], str(input('receiver email')), 'google.com')
+send_email.send_email(credentials[0], credentials[1], str(input('receiver email?  ')), 'https://www.google.com/')
 print('Completed')
