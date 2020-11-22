1a44b6dd59ff5fe7bae345a9f11efc7eaf48692f
NinjadenMu <jaden.mu@gmail.com>
2020-11-22 11:17:05 -0500

    Create send_email_tester.py

---
+++ send_email_tester.py
@@ -0,0 +1,6 @@
+import send_email
+
+credentials = str(open("email_config.txt", "r").read())
+credentials = credentials.split('\n')
+send_email.send_email(credentials[0], credentials[1], str(input('receiver email')), 'google.com')
+print('Completed')
