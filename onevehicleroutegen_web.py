e819ddd8c62eb6cf3663d3ee33af2c93c0799408
Yikuan Sun <yikuansun2015@gmail.com>
2020-12-09 20:30:00 -0500

    fixed fast mode distance

--- onevehicleroutegen_web.py
+++ onevehicleroutegen_web.py
@@ -8,6 +8,7 @@ from ortools.constraint_solver import pywrapcp
 import string
 import random
 import pickle
+import math
 
 def take_inputs(api_key, fakeinputfile):
 
@@ -53,7 +54,12 @@ def take_inputs(api_key, fakeinputfile):
     return (addresses, coords)
 
 def fast_mode_distance(coords1, coords2):
-    return ( ((coords1[0] - coords2[0]) ** 2 + (coords1[1] - coords2[1]) ** 2) ** 0.5 ) * 140000 # swell the distances such that the tsp algorithm doesn't read them as all zeroes
+    # convert coords to meters
+    coords1[0] = coords1[0] * 111320
+    coords2[0] = coords2[0] * 111320
+    coords1[1] = coords1[1] * 40075000 * math.cos(coords1[0] * math.pi / 180) / 360
+    coords2[1] = coords2[1] * 40075000 * math.cos(coords2[0] * math.pi / 180) / 360
+    return ( ((coords1[0] - coords2[0]) ** 2 + (coords1[1] - coords2[1]) ** 2) ** 0.5 )
 
 
 def generate_distance_matrix(api_key, fakeinputfile, fast_mode_toggled):
