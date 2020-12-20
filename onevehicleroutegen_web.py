f8dcff083a7388c9bf4e0c4c31be8d1c4b51065c
Yikuan Sun <yikuansun2015@gmail.com>
2020-12-19 22:05:06 -0500

    Update onevehicleroutegen_web.py

--- onevehicleroutegen_web.py
+++ onevehicleroutegen_web.py
@@ -10,6 +10,10 @@ import random
 import pickle
 import math
 
+import urllib
+import requests
+import json
+
 def take_inputs(api_key, fakeinputfile):
 
     geolocator = gmaps.Client(key=api_key)
@@ -62,42 +66,27 @@ def fast_mode_distance(coords1, coords2):
     lat2 = coords2[0] * (111132.954  - 559.822 * math.cos(2 * coords2[0] * math.pi / 180) + 1.175 * math.cos(4 * coords2[0] * math.pi / 180))
     return ((lat1 - lat2) ** 2 + (lon1 - lon2) ** 2) ** 0.5
 
-def generate_distance_matrix(coords, fast_mode_toggled):
+def generate_distance_matrix(addresses, api_key):
     MAX_DISTANCE = 7666432.01 # a constant rigging distance matrix to force the optimizer to go to origin first
 
-    # initiate vars
-    if not fast_mode_toggled:
-        # load previously saved graph; unneeded if not fast mode
-        G = pickle.load(open("graph", "rb"))
-    else:
-        G = "who cares really (if fast mode is off)? this is just to prevent error"
-
     # create 2d array with distances of node i -> node j
-    if fast_mode_toggled:
-        output_list = []
-        for i in range(len(coords)):
-            output_list.append([])
-            for j in range(len(coords)):
-                output_list[i].append(fast_mode_distance(coords[i], coords[j]))
-        # rig distance so that optimization algorithm chooses to go to origin asap (after depot)
-        for i in range(2, len(output_list)):
-            output_list[i][1] = MAX_DISTANCE
-    else:
-        # Generate nodes
-        nodes = []
-        for i in range(len(coords)):
-            nodes.append(ox.get_nearest_node(G, coords[i]))
-
-        output_list = []
-        
-        for i in range(len(nodes)):
-            output_list.append([])
-            for j in range(len(nodes)):
-                output_list[i].append(nx.shortest_path_length(G, nodes[i], nodes[j], weight='length'))
-    
-        # rig distance so that optimization algorithm chooses to go to origin asap (after depot)
-        for i in range(2, len(output_list)):
-            output_list[i][1] = MAX_DISTANCE
+    output_list = []
+    for i in range(len(addresses)):
+        output_list.append([])
+        for j in range(len(addresses)):
+            print(
+                json.loads(
+                    requests.get("https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=" + urllib.parse.quote_plus(addresses[i] + "|" + addresses[j]) + "&key=" + api_key).text
+                )
+            )
+            output_list[i].append(
+                json.loads(
+                    requests.get("https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=" + urllib.parse.quote_plus(addresses[i] + "|" + addresses[j]) + "&key=" + api_key).text
+                )["rows"][0]["elements"][0]["distance"]["value"]
+            )
+    # rig distance so that optimization algorithm chooses to go to origin asap (after depot)
+    for i in range(2, len(output_list)):
+        output_list[i][1] = MAX_DISTANCE
     
     # output data
     return (output_list)
@@ -136,33 +125,28 @@ def print_solution(manager, routing, solution, addresses):
 
 def main(api_key, fakeinputfile, fast_mode_toggled):
     #process addresses and check for faulty ones
-    faultyAddress, addresses, coords = take_inputs(api_key, fakeinputfile)
-    if len(faultyAddress) == 0:
-        # run ORTools
-        distancematrix = generate_distance_matrix(coords, fast_mode_toggled)
-        data = create_data_model(distancematrix)
-        manager = pywrapcp.RoutingIndexManager(len(data['distance_matrix']),
-                                                data['num_vehicles'], data['depot'])
-        routing = pywrapcp.RoutingModel(manager)
-        def distance_callback(from_index, to_index):
-            from_node = manager.IndexToNode(from_index)
-            to_node = manager.IndexToNode(to_index)
-            return data['distance_matrix'][from_node][to_node]
-        transit_callback_index = routing.RegisterTransitCallback(distance_callback)
-        routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
-        search_parameters = pywrapcp.DefaultRoutingSearchParameters()
-        search_parameters.first_solution_strategy = (routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
-        solution = routing.SolveWithParameters(search_parameters)
-        route_solution, stringoutput = print_solution(manager, routing, solution, addresses)
-        if solution:
-            route_solution
-        #print(route_solution)
-        return (route_solution.replace("->", " -><br>"), stringoutput)
-    else:
-        output = "<h1>Incorrect address(es)</h1>"
-        for address in faultyAddress:
-            output += "<p style=\"color:Tomato;\">" + address + "</p>"
-        return(output, "")
+    #faultyAddress, addresses, coords = take_inputs(api_key, fakeinputfile)
+
+    # run ORTools
+    distancematrix = generate_distance_matrix(fakeinputfile.splitlines(), api_key)
+    data = create_data_model(distancematrix)
+    manager = pywrapcp.RoutingIndexManager(len(data['distance_matrix']),
+                                            data['num_vehicles'], data['depot'])
+    routing = pywrapcp.RoutingModel(manager)
+    def distance_callback(from_index, to_index):
+        from_node = manager.IndexToNode(from_index)
+        to_node = manager.IndexToNode(to_index)
+        return data['distance_matrix'][from_node][to_node]
+    transit_callback_index = routing.RegisterTransitCallback(distance_callback)
+    routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
+    search_parameters = pywrapcp.DefaultRoutingSearchParameters()
+    search_parameters.first_solution_strategy = (routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
+    solution = routing.SolveWithParameters(search_parameters)
+    route_solution, stringoutput = print_solution(manager, routing, solution, fakeinputfile.splitlines())
+    if solution:
+        route_solution
+    #print(route_solution)
+    return (route_solution.replace("->", " -><br>"), stringoutput)
 
 if __name__ == '__main__':
     # run the main script
