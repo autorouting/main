d13b18bff0949882d9c6d388ed8ce178d17d15f1
Yikuan Sun <yikuansun2015@gmail.com>
2021-01-23 20:55:35 -0500

    made exception for writing to database

--- database.py
+++ database.py
@@ -44,8 +44,14 @@ def insert_data(InputAddress, PlaceID, lon, lat, FormattedAddress, OSMnode):
     input_code = "INSERT INTO userinput (inputaddress, placeid) VALUES (%s, %s)"
     output_code = "INSERT INTO maptable (placeid, coorx, coory, googleaddresses, openmnode) VALUES (%s, %s, %s, %s, %s)"
 
-    mycursor.execute(input_code, (InputAddress, PlaceID,))
-    mycursor.execute(output_code, (PlaceID, lon, lat, FormattedAddress, OSMnode))
+    try:
+        mycursor.execute(input_code, (InputAddress, PlaceID,))
+    except mysql.connector.Error as err:
+        print("Something went wrong: " + err)
+    try:
+        mycursor.execute(output_code, (PlaceID, lon, lat, FormattedAddress, OSMnode))
+    except mysql.connector.Error as err:
+        print("Something went wrong: " + err)
 
     mydb.commit()
 
