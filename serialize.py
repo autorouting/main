b7169057a4e6ff2525a31f303cd36da996877dbe
amwang9276 <amwangpanda@gmail.com>
2021-03-06 21:18:26 -0500

    Update serialize.py

--- serialize.py
+++ serialize.py
@@ -1,10 +1,9 @@
-#ending character: ?
-
-def serializeCgiToServer(coors): #coors is a n by 2 2D array
+def serializeCgiToServer(coors): #coors is a n by 2 2D array of doubles
     string = ""
     for i in range(0, len(coors)):
-        string += str(coors[i][0]) + "," + str(coors[i][1])
-        string += ";"
+        string += str(coors[i][0]) + "," + str(coors[i][1]) # add comma between x and y coor
+        string += ";" # add semicolon to seperate coors
+    string += "\n" #ending character
     return string.encode('utf-8')
     
 def deserializeCgiToServer(string):
@@ -14,15 +13,16 @@ def deserializeCgiToServer(string):
     coors = [None]*(len(splitted)-1) # create list: [None, None, None,...] with length len(splitted)-1
     for i in range(0,len(splitted)-1):
         coors[i] = splitted[i].split(",")
-        coors[i] = [float(x) for x in coors[i]]
+        coors[i] = [float(x) for x in coors[i]] # convert to float
     return coors
 
-def serializeServerToCgi(distanceMatrix):
+def serializeServerToCgi(distanceMatrix): # distanceMatrix is matrix of doubles
     string = ""
     for i in range(0, len(distanceMatrix)):
         for j in range(0, len(distanceMatrix[i])):
-            string += str(distanceMatrix[i][j]) + ","
-        string += ";"
+            string += str(distanceMatrix[i][j]) + "," # seperate distances with comma
+        string += ";" # seperate rows with semicolon
+    string += "\n" # ending character
     return string.encode('utf-8')
     
 def deserializeServerToCgi(string):
@@ -33,7 +33,7 @@ def deserializeServerToCgi(string):
     for i in range(0,len(splitted)-1):
         row = splitted[i].split(",")
         distanceMatrix[i] = row[0:len(row)-1]
-        distanceMatrix[i] = [float(x) for x in distanceMatrix[i]]
+        distanceMatrix[i] = [float(x) for x in distanceMatrix[i]] # convert to float
     return distanceMatrix
     
 def testFunctions():
