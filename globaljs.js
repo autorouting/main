15d84474bdd1e8e806b7eab0d25e3b31fcda722e
Yikuan Sun <yikuansun2015@gmail.com>
2021-04-11 16:54:21 -0400

    cleaned stuff up

---
+++ globaljs.js
@@ -0,0 +1,37 @@
+function googleTranslateElementInit() {
+    new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+}
+            window.formbutton=window.formbutton||function(){(formbutton.q=formbutton.q||[]).push(arguments)};
+formbutton("create", {
+    action: "https://formspree.io/f/xdopqwzj",
+    title: "Comments?", 
+    description: "Send 'em in!",
+    fields: [
+    { 
+        type: "email", 
+        label: "Email:",
+        name: "_replyto",
+        required: true,
+        placeholder: "joshswain@example.com"
+    },
+    {
+        type: "textarea",
+        label: "Message:",
+        name: "message",
+        required: true,
+        placeholder: "I found a bug!",
+    },
+    { type: "submit" }      
+    ],
+    styles: {
+        title: {
+            backgroundColor: "#222222"
+        },
+        description: {
+            backgroundColor: "#222222"
+        },
+        button: {
+            backgroundColor: "#222222"
+        }
+    }
+});
