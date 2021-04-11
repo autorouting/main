f6435aee6c49b74da2ed73d36a7cf5f56ef006a9
Yikuan Sun <yikuansun2015@gmail.com>
2021-04-11 16:50:18 -0400

    added the stuff

--- output.js
+++ output.js
@@ -2,20 +2,21 @@ $(document).ready(function() {
     $.getJSON(location.origin + "/cgi-bin/webgui.py" + location.search, function(data) {
         switch (data.status) {
             case ("made_route"):
+                document.querySelector("#containerbox").innerHTML = "";
                 console.log(data);
                 var solution_display = document.createElement("p");
                 solution_display.innerHTML = data.route_solution;
-                document.body.appendChild(solution_display);
+                document.querySelector("#containerbox").appendChild(solution_display);
                 var route_link = decodeURIComponent(data.route_link);
                 var a = document.createElement("a");
                 a.href = route_link;
                 a.innerText = "Open Google Maps link";
-                document.body.appendChild(a);
+                document.querySelector("#containerbox").appendChild(a);
                 var br = document.createElement("br");
-                document.body.appendChild(br);
+                document.querySelector("#containerbox").appendChild(br);
                 var qrcode = new Image();
                 qrcode.src = "https://api.qrserver.com/v1/create-qr-code/?size=300x300&data=" + data.route_link;
-                document.body.appendChild(qrcode);
+                document.querySelector("#containerbox").appendChild(qrcode);
                 break;
         }
     });
