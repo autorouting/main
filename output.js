964e99c5d96c538340c5314f2b86f630249252b1
Yikuan Sun <yikuansun2015@gmail.com>
2021-04-11 16:40:51 -0400

    simple ui

--- output.js
+++ output.js
@@ -1,5 +1,22 @@
 $(document).ready(function() {
     $.getJSON(location.origin + "/cgi-bin/webgui.py" + location.search, function(data) {
-            console.log(data);
+        switch (data.status) {
+            case ("made_route"):
+                console.log(data);
+                var solution_display = document.createElement("p");
+                solution_display.innerHTML = data.route_solution;
+                document.body.appendChild(solution_display);
+                var route_link = decodeURIComponent(data.route_link);
+                var a = document.createElement("a");
+                a.href = route_link;
+                a.innerText = "Open Google Maps link";
+                document.body.appendChild(a);
+                var br = document.createElement("br");
+                document.body.appendChild(br);
+                var qrcode = new Image();
+                qrcode.src = "https://api.qrserver.com/v1/create-qr-code/?size=300x300&data=" + data.route_link;
+                document.body.appendChild(qrcode);
+                break;
+        }
     });
 });
