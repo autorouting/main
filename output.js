4618d6683bbca2ccb92e239f6df62020ad8145a0
Yikuan Sun <yikuansun2015@gmail.com>
2021-04-11 19:27:33 -0400

    made the code clean

--- output.js
+++ output.js
@@ -7,15 +7,16 @@ $(document).ready(function() {
                 var solution_display = document.createElement("p");
                 solution_display.innerHTML = data.route_solution;
                 document.querySelector("#containerbox").appendChild(solution_display);
-                var route_link = decodeURIComponent(data.route_link);
+                var route_link = data.route_link;
                 var a = document.createElement("a");
                 a.href = route_link;
                 a.innerText = "Open Google Maps link";
+                a.style.fontWeight = "bold";
                 document.querySelector("#containerbox").appendChild(a);
                 var br = document.createElement("br");
                 document.querySelector("#containerbox").appendChild(br);
                 var qrcode = new Image();
-                qrcode.src = "https://api.qrserver.com/v1/create-qr-code/?size=300x300&data=" + data.route_link;
+                qrcode.src = "https://api.qrserver.com/v1/create-qr-code/?size=300x300&data=" + encodeURIComponent(route_link);
                 document.querySelector("#containerbox").appendChild(qrcode);
                 break;
             case ("invalid_address"):
