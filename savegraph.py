7c19454c8fef0e8e1cd6a0c26a28abd56b14bfb0
Yikuan Sun <yikuansun2015@gmail.com>
2021-05-03 19:34:28 -0400

    Update savegraph.py

--- savegraph.py
+++ savegraph.py
@@ -1,5 +1,5 @@
 import pickle
 import osmnx as ox
 point = (35.8972385, -78.8627028)
-streets_graph = ox.graph.graph_from_point(point, dist=80467.2, network_type='drive', simplify=False)
+streets_graph = ox.graph.graph_from_point(point, dist=80000, network_type='drive', simplify=False)
 pickle.dump(streets_graph, open("graph", "wb"))
