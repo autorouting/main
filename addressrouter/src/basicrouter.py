4fdc4e3ac5753d4539785f31bd0df0efce2ae3d3
NinjadenMu <jaden.mu@gmail.com>
2021-07-23 18:00:09 -0400

    idk why but the order of importing libraries matters apparently.  just switched the order.  no biggie so no pull reqeuest

--- addressrouter/src/basicrouter.py
+++ addressrouter/src/basicrouter.py
@@ -1,6 +1,6 @@
 #Import libraries
-from ortools.constraint_solver import routing_enums_pb2
 from ortools.constraint_solver import pywrapcp
+from ortools.constraint_solver import routing_enums_pb2
 import string
 import random
 import pickle
