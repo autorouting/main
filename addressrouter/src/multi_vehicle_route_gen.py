355be866a0fb63643d9d0499c04ea1ad04a096cb
amwang9276 <69082014+amwang9276@users.noreply.github.com>
2021-08-07 19:14:38 -0400

    Update multi_vehicle_route_gen.py

--- addressrouter/src/multi_vehicle_route_gen.py
+++ addressrouter/src/multi_vehicle_route_gen.py
@@ -10,25 +10,6 @@ class solve_multi(basicrouter.BasicRouter):
         self._numvehicles = num_vehicles
         #force_fairness does nothing right now
         self.force_fairness = force_fairness
-
-    def print_solution(self, data, manager, routing, solution):
-        """Prints solution on console."""
-        print(f'Objective: {solution.ObjectiveValue()}')
-        max_route_distance = 0
-        for vehicle_id in range(data['num_vehicles']):
-            index = routing.Start(vehicle_id)
-            plan_output = 'Route for vehicle {}:\n'.format(vehicle_id)
-            route_distance = 0
-            while not routing.IsEnd(index):
-                plan_output += ' {} -> '.format(manager.IndexToNode(index))
-                previous_index = index
-                index = solution.Value(routing.NextVar(index))
-                route_distance += routing.GetArcCostForVehicle(
-                    previous_index, index, vehicle_id)
-            plan_output += '{}\n'.format(manager.IndexToNode(index))
-            plan_output += 'Distance of the route: {}m\n'.format(route_distance)
-            print(plan_output)
-            max_route_distance = max(route_distance, max_route_distance)
         
         print('Maximum of the route distances: {}m'.format(max_route_distance))
 
@@ -82,6 +63,25 @@ class solve_multi(basicrouter.BasicRouter):
 
         # Print solution on console.
         if solution:
+            def print_solution(data, manager, routing, solution):
+                """Prints solution on console."""
+                print(f'Objective: {solution.ObjectiveValue()}')
+                max_route_distance = 0
+               for vehicle_id in range(data['num_vehicles']):
+                    index = routing.Start(vehicle_id)
+                    plan_output = 'Route for vehicle {}:\n'.format(vehicle_id)
+                    route_distance = 0
+                    while not routing.IsEnd(index):
+                        plan_output += ' {} -> '.format(manager.IndexToNode(index))
+                        previous_index = index
+                        index = solution.Value(routing.NextVar(index))
+                        route_distance += routing.GetArcCostForVehicle(
+                            previous_index, index, vehicle_id)
+                    plan_output += '{}\n'.format(manager.IndexToNode(index))
+                    plan_output += 'Distance of the route: {}m\n'.format(route_distance)
+                    print(plan_output)
+                    max_route_distance = max(route_distance, max_route_distance)
+            
             self.print_solution(data, manager, routing, solution)
         
         else:
