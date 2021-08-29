889f291092874b68038fb99c2764e33933eb8bcf
yikuansun <yikuansun2015@gmail.com>
2021-08-29 13:55:15 -0400

    Create key file if not exist

--- addressrouter/tests/basictester.py
+++ addressrouter/tests/basictester.py
@@ -4,6 +4,15 @@ sys.path.append(path.dirname(path.dirname(path.abspath(__file__)))+"/src")
 from basicrouter import BasicRouter
 import maputil
 
+if path.isfile(path.dirname(path.abspath(__file__)) + "/testfiles/api.txt"):
+    # API Key file exists
+    pass
+else:
+    # Need to make API Key file
+    f = open(path.dirname(path.abspath(__file__)) + "/testfiles/api.txt", "w")
+    f.write(input("API Key????\n > "))
+    f.close()
+
 testnum=3
 myRouter = BasicRouter(
     open(path.dirname(path.abspath(__file__)) + "/testfiles/test{}.txt".format(testnum)).read().splitlines(),
