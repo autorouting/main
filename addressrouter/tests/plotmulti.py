db64ee73c283ccd3e72b4453d025dbfcbca29dd2
yikuansun <yikuansun2015@gmail.com>
2021-09-30 22:14:47 -0400

    Create plotmulti.py

---
+++ addressrouter/tests/plotmulti.py
@@ -0,0 +1,10 @@
+import simplemulti
+import matplotlib.pyplot as plt
+
+testnum = 1
+result = simplemulti.run_multivehicle(testnum)
+
+for vehicle in result:
+    pass # Must pass PR#282
+
+plt.show()
