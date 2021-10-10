ee2cc6e751db8f4a88eff1ab1267b9898863b679
Yikuan Sun <yikuansun2015@gmail.com>
2021-10-09 20:51:32 -0400

    Fix plotmulti

--- addressrouter/tests/plotmulti.py
+++ addressrouter/tests/plotmulti.py
@@ -8,22 +8,21 @@ result = simplemulti.run_multivehicle(testnum)
 color_spectrum = ["red", "orange", "yellow", "green", "blue", "purple"]
 color_index = 0
 
-for vehicle in result:
-    for ordered_coordinates in result[1]:
-        x_coordinates = [row[1] for row in ordered_coordinates]
-        y_coordinates = [row[0] for row in ordered_coordinates]
+for ordered_coordinates in result[1]:
+    x_coordinates = [row[1] for row in ordered_coordinates]
+    y_coordinates = [row[0] for row in ordered_coordinates]
 
-        #plot origin
-        plt.plot(x_coordinates[0], y_coordinates[0], 'k>', markersize=9)
-        #plot destination
-        plt.plot(x_coordinates[-1], y_coordinates[-1], 'k<', markersize=9)
-        #plot intermediate addresses
-        plt.plot(x_coordinates[1:len(result[1])-1], y_coordinates[1:len(result[1])-1], 'ko', markersize=4)
-        #plot the tour using lines
-        plt.plot(x_coordinates, y_coordinates, color=color_spectrum[color_index], linestyle='-')
+    #plot origin
+    plt.plot(x_coordinates[0], y_coordinates[0], 'kD', markersize=9)
+    #plot destination
+    plt.plot(x_coordinates[-1], y_coordinates[-1], 'kx', markersize=9)
+    #plot intermediate addresses
+    plt.plot(x_coordinates[1:len(ordered_coordinates)-1], y_coordinates[1:len(ordered_coordinates)-1], 'ko', markersize=4, color=color_spectrum[color_index])
+    #plot the tour using lines
+    plt.plot(x_coordinates, y_coordinates, color=color_spectrum[color_index], linestyle='-')
 
-        # Use a new color for next tour
-        color_index += 1
-        color_index = color_index % len(color_spectrum)
+    # Use a new color for next tour
+    color_index += 1
+    color_index = color_index % len(color_spectrum)
 
 plt.show()
