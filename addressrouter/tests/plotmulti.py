54d6ee46236c2f3ccd5161844e1d958615bd6048
yikuansun <yikuansun2015@gmail.com>
2021-11-14 17:30:19 -0500

    Improve symbols for plotmulti

--- addressrouter/tests/plotmulti.py
+++ addressrouter/tests/plotmulti.py
@@ -13,9 +13,9 @@ for ordered_coordinates in result[1]:
     y_coordinates = [row[0] for row in ordered_coordinates]
 
     #plot origin
-    plt.plot(x_coordinates[0], y_coordinates[0], 'kD', markersize=9, color=color_spectrum[color_index])
+    plt.plot(x_coordinates[0], y_coordinates[0], 'k*', markersize=9, color=color_spectrum[color_index])
     #plot destination
-    plt.plot(x_coordinates[-1], y_coordinates[-1], 'kx', markersize=9, color=color_spectrum[color_index])
+    plt.plot(x_coordinates[-1], y_coordinates[-1], 'k8', markersize=9, color=color_spectrum[color_index])
     #plot intermediate addresses
     plt.plot(x_coordinates[1:len(ordered_coordinates)-1], y_coordinates[1:len(ordered_coordinates)-1], 'ko', markersize=4, color=color_spectrum[color_index])
     #plot the tour using lines
